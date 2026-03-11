4fd696e64bb777897565f619e25828a391391a9f
ArtCer <760a.cerniauskas@pix-pro.com>
2026-03-11 09:55:07 +0200

    *Add methods to repository

--- src/main/java/com/example/demo/repositories/ExchangeRateRepository.java
+++ src/main/java/com/example/demo/repositories/ExchangeRateRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repositories;
 
 import com.example.demo.entities.ExchangeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -13,10 +14,18 @@ public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, UUID
 
     List<ExchangeRate> findByDateOfRate(LocalDate date);
 
-    List<ExchangeRate> findByCurrencyCodeOrderByDateOfRateAsc(String currencyCode);
-
-    boolean existsByDateOfRate(LocalDate date);
+    List<ExchangeRate> findByCurrencyCodeAndDateOfRateBetweenOrderByDateOfRateDesc(
+            String currencyCode, LocalDate from, LocalDate to);
 
     ExchangeRate findTopByOrderByDateOfRateDesc();
 
+    @Query("SELECT er.dateOfRate FROM ExchangeRate er WHERE er.currencyCode = :currencyCode")
+    List<LocalDate> findDatesByCurrencyCode(String currencyCode);
+
+    boolean existsByCurrencyCodeAndDateOfRate(String currencyCode, LocalDate dateOfRate);
+
+    @Query("SELECT DISTINCT e.currencyCode FROM ExchangeRate e ORDER BY e.currencyCode")
+    List<String> findAllCurrencyCodes();
+
+    ExchangeRate findTopByCurrencyCodeOrderByDateOfRateDesc(String currencyCode);
 }
